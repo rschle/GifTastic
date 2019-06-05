445d34c99193d17e86eaf78797088f8814e45a7a
Rebecca Schleimer <rschle@sas.upenn.edu>
2019-06-05 13:58:12 -0700

    working on gif pausing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,60 +2,117 @@
 //adding a new show button to the array when we type and submit
 var shows = ["Stranger Things", "Orange is the New Black", "House of Cards", "Arrested Development", "Umbrella Academy", "Black Mirror", "Jessica Jones"]
 
-function renderButtons(){
 
-    $("#button-view").empty();
-
-    for(var i = 0; i < shows.length; i++){
-        var a = $("<button>");
-        a.addClass("show");
-        a.attr("data-name", shows[i]);
-        a.text(shows[i]);
-        $("#button-view").append(a);
-    }
-}
-
-$("#add-show").on("click", function(event){
-    event.preventDefault();
-
-    var show = $("#netflixInput").val().trim();
-    shows.push(show);
-
+$(document).ready(function(){
     renderButtons();
-})
+    
+    function renderButtons(){
+        
+        $("#button-view").empty();
+    
+        for(var i = 0; i < shows.length; i++){
+            var a = $("<button>");
+            a.addClass("show-btn");
+            a.attr("data-name", shows[i]);
+            a.text(shows[i]);
+            $("#button-view").append(a);
+        }
+    }
+    
+    $("#add-show").on("click", function(event){
+        event.preventDefault();
+    
+        var show = $("#netflixInput").val().trim();
+        shows.push(show);
+    
+        renderButtons();
+    });
+    
+    $(document).on("click", ".show-btn", displayShowInfo);
 
-renderButtons();
-
-
-$("button").on("click", function(){
+function displayShowInfo(){
     var show = $(this).attr("data-name")
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=o3PAEwYAONHFsq5wlEb3PuiE5qWGm4PP";
-
-$.ajax({
+    
+    $.ajax({
     url: queryURL,
     method: "GET"
-}).then(function(response) {
+    }).then(function(response) {
+    $("#gifs-appear-here").html("");
     console.log(response);
-    var results = response.data;
+    
 
 
-    for (var i = 0; i < results.length; i++){
-        var showDiv = $("<div>");
-        var rating = results[i].rating;
-        var p = $("<p>").text("Rating: " + rating);
+
+    for (var i = 0; i < response.data.length; i++){
+       
     
         var showImage = $("<img>");
-        showImage.attr("src", results[i].images.fixed_height.url);
+        showImage.attr("src", response.data[i].images.fixed_height.url);
+        showImage.attr("data-animate", response.data[i].images.fixed_height.url);
+        showImage.attr("data-still", response.data[i].images.fixed_height.url);
+        showImage.addClass("gif")
+        showImage.attr("data-state", "still")
+        
+        
+
+        var rating = response.data[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+
+        // showDiv.html(p);
+        var pDiv = $("<div class='ratingDiv'>")
+        pDiv.text("Rating: " + response.data[i].rating.toUpperCase());
+        //showDiv.html(showImage);
+        var allDiv = $("<div class='allDiv'>")
+        allDiv.append(pDiv);
+        allDiv.append(showImage);
+        $("#gifs-appear-here").prepend(allDiv);
+
+
+        //making image on-click 
+
     
-        showDiv.append(p);
-        showDiv.append(showImage);
+       
+        }
+
     
-        $("#gifs-appear-here").prepend(showDiv);
+     });
     }
-});
 
 
-
+    $("#gifs-appear-here").on("click", ".gif", function(){
+        var state = $(this).attr("data-state");
+    //     var animate = $(this).attr("data-animate");
+    //     var still = $(this).attr("data-still")
+    //     console.log(state);
+    //setting conditional statements so we know when the gif is paused/animated etc.
+        if(state === "animate"){
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+            console.log(state);
+        }else if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }
+    
+    });
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
